a5bf504918a2e49d075c0c13497ea3bc58ebf5be
Dario L Garcia <dario.garcia@10pines.com>
2018-02-20 22:28:26 -0300

    Ordenar lista por nombre

--- app/components/lista-de-menues.js
+++ app/components/lista-de-menues.js
@@ -1,3 +1,10 @@
 import Component from '@ember/component';
+import {computed} from '@ember/object';
 
-export default Component.extend({});
+
+export default Component.extend({
+
+  menuesOrdenados: computed('menues.[]', function () {
+    return this.get('menues').sortBy('comensal.nombre');
+  }),
+});
